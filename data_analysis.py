1f782a6cf895e5e462e627628edd0d43e8aa1da1
Nguyen Hai Dang <nguyenhaidang04072004@gmail.com>
2024-08-15 09:19:45 +0700

    data analysis

---
+++ data_analysis.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+file_path = './data/Student_performance_data _.csv'
+df = pd.read_csv(file_path)
+# print(df.head())
+# print(df.shape)
+
+# print(df.describe())
+# print(df.info())
+# print(df.isnull().sum()) # check null
+
