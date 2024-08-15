6cb60cf8006bbf2659909bf6ce3dabd96eafcd4b
Nguyen Hai Dang <nguyenhaidang04072004@gmail.com>
2024-08-15 19:10:01 +0700

    add train

---
+++ train.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('./data/Student_performance_data _.csv')
+print(df.head())
+data = df.drop(['StudentID', 'Ethnicity', 'GPA', 'GradeClass'], axis=1)
+target = df.GPA
+
+print(data)
+print(target)
+
+x_train, x_x, y_train, y_y = train_test_split(data, target, test_size=0.4)
+x_val, x_test, y_val, y_test = train_test_split(x_x, y_y, test_size=0.2)
+
